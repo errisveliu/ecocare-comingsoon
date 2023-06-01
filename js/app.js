ff345ed8604acd40f3d6878d7b0f581c78fbd631
Erris Veliu <errisveliu@gmail.com>
2023-06-01 18:28:04 +0200

    Final Version

--- js/app.js
+++ js/app.js
@@ -2,49 +2,62 @@ document.addEventListener('DOMContentLoaded', function() {
     var hideLink = document.getElementById('hide1');
     var showLink = document.getElementById('show1');
     var weiterlesen = document.getElementById('weiterlesen');
-    var newsletterButton = document.getElementById('newsletterButton');
-    var emailInput = document.getElementById('emailInput');
-    var emailError = document.getElementById('emailError');
-    var clicked = false;
+    var expanded = false;
   
     hideLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      if (!clicked) {
+      event.preventDefault(); 
+  
+      if (!expanded) {
         showLink.style.display = 'block';
         weiterlesen.style.display = 'none';
-        clicked = true;
-        newsletterButton.innerHTML = 'Abonniert';
+        expanded = true;
       } else {
         showLink.style.display = 'none';
         weiterlesen.style.display = 'inline';
-        clicked = false;
-        newsletterButton.innerHTML = 'Newsletter abonnieren';
+        expanded = false;
       }
     });
   
     showLink.addEventListener('click', function(event) {
       event.preventDefault();
+  
       showLink.style.display = 'none';
       weiterlesen.style.display = 'inline';
-      clicked = false;
-      newsletterButton.innerHTML = 'Newsletter abonnieren';
+      expanded = false;
     });
   
+    var emailInput = document.getElementById('emailInput');
+    var emailError = document.getElementById('emailError');
+    var newsletterButton = document.getElementById('newsletterButton');
+    var isSubscribed = false;
+  
     emailInput.addEventListener('input', function() {
-      if (!emailInput.validity.valid) {
-        emailError.style.display = 'block';
-        newsletterButton.disabled = true;
+        if (!emailInput.validity.valid) {
+          emailError.style.display = 'block';
+          newsletterButton.disabled = true;
+          newsletterButton.style.backgroundColor = '#FC5E4D';
+        } else {
+          emailError.style.display = 'none';
+          newsletterButton.disabled = false;
+          newsletterButton.style.backgroundColor = '#26EA8F';
+        }
+      });
+  
+    newsletterButton.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      if (isSubscribed) {
+        newsletterButton.textContent = 'Newsletter abonnieren';
         newsletterButton.style.backgroundColor = '#FC5E4D';
+        isSubscribed = false;
       } else {
-        emailError.style.display = 'none';
-        newsletterButton.disabled = false;
-        newsletterButton.style.backgroundColor = '#26EA8F';
+        newsletterButton.textContent = 'Abonniert';
+        isSubscribed = true;
       }
     });
   
     emailInput.addEventListener('blur', function() {
-      if (!emailInput.validity.valid) {
-        emailError.style.display = 'none';
+      if (emailInput.value === '') {
         newsletterButton.disabled = true;
         newsletterButton.style.backgroundColor = '#FC5E4D';
       }
